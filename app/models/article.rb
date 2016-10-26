d19fe4cd15d086ef301cbf3ca67d1a00fa0b7f70
Rick <rbciti@msn.com>
2016-10-25 17:13:42 -0700

    Add validations to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
-class Article  < ActiveRecord::Base  
+class Article  < ActiveRecord::Base 
+
+validates_presence_of :title, length: {minimum: 5, maximun: 70}
+validates_presence_of :description, length: {minimum: 10, maximun: 300} 
 
 end
